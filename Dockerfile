24743aa8e1e3c788c6a271274028df288a84b841
Ilya Makoveev <crlnpro@yandex.ru>
2024-11-17 15:17:48 +0300

    fix: upd logging in console

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 FROM mcr.microsoft.com/dotnet/aspnet:6.0 AS base
 WORKDIR /app
 
-EXPOSE 80
-EXPOSE 443
-
 FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
 WORKDIR /src
 COPY . .
@@ -16,4 +13,7 @@ RUN dotnet publish -c Release -o /app/publish
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
+
+EXPOSE 80
+EXPOSE 443
 ENTRYPOINT ["dotnet", "Backend.dll"]
--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -1,6 +1,5 @@
 ﻿using Backend.Models;
 using MailKit.Net.Smtp;
-using MailKit.Security;
 using MimeKit;
 using MimeKit.Text;
 
@@ -9,26 +8,44 @@ namespace Backend.Services
     public class EmailService : IEmailService
     {
         private readonly IConfiguration _configuration;
+        private readonly ILogger<EmailService> _logger;
 
-        public EmailService(IConfiguration configuration)
+        public EmailService(ILogger<EmailService> logger, IConfiguration configuration)
         {
+            _logger = logger;
             _configuration = configuration;
         }
 
         public async Task SendEmailAsync(EmailDto emailDto)
         {
-            var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse("info@softbox-solution.ru"));
-            email.To.Add(MailboxAddress.Parse(emailDto.To));
-            email.Subject = emailDto.Subject;
-            email.Body = new TextPart(TextFormat.Text) { Text = emailDto.Body };
-
-            using (var smtp = new SmtpClient())
+            try
             {
-                await smtp.ConnectAsync(_configuration["EmailHost"], 465, true);
-                await smtp.AuthenticateAsync(_configuration["EmailUsername"], _configuration["EmailPassword"]);
-                await smtp.SendAsync(email);
-                await smtp.DisconnectAsync(true);
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse("info@softbox-solution.ru"));
+                email.To.Add(MailboxAddress.Parse(emailDto.To));
+                email.Subject = emailDto.Subject;
+                email.Body = new TextPart(TextFormat.Text) { Text = emailDto.Body };
+                
+                _logger.LogInformation("Prepare to send email to {TO} with subject {Subject}",emailDto.To, emailDto.Subject);
+
+                using (var smtp = new SmtpClient())
+                {
+                    _logger.LogInformation("Connecting to SMTP server {EmailHost}", _configuration["EmailHost"]);
+                    await smtp.ConnectAsync(_configuration["EmailHost"], 465, true);
+                    
+                    _logger.LogInformation("Authenticating with SMTP server");
+                    await smtp.AuthenticateAsync(_configuration["EmailUsername"], _configuration["EmailPassword"]);
+                    
+                    _logger.LogInformation("Sending email");
+                    await smtp.SendAsync(email);
+                    
+                    _logger.LogInformation("Disconnecting from SMTP server");
+                    await smtp.DisconnectAsync(true);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to send email to {TO}", emailDto.To);
             }
         }
     }
